bb383563249d632ec8f9026e2ee65271105bc54b
Adam Wójs <adam@wojs.pl>
2022-07-20 10:51:03 +0200

    Fixed code style issues

--- src/lib/Handler.php
+++ src/lib/Handler.php
@@ -478,9 +478,6 @@ class Handler implements VersatileHandler
             case SearchService::CAPABILITY_SCORING:
             case SearchService::CAPABILITY_FACETS:
             case SearchService::CAPABILITY_CUSTOM_FIELDS:
-            //case SearchService::CAPABILITY_SPELLCHECK:
-            //case SearchService::CAPABILITY_HIGHLIGHT:
-            //case SearchService::CAPABILITY_SUGGEST:
             case SearchService::CAPABILITY_ADVANCED_FULLTEXT:
             case SearchService::CAPABILITY_AGGREGATIONS:
                 return true;
