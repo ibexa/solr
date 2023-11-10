ebbfdfeb134de0d65491ac93734207929c749595
Adam Wójs <adam@wojs.pl>
2023-11-10 07:38:37 +0100

    fixup! Skipped generating solrconfig.xml file copies in generate-solr-config.sh

--- bin/generate-solr-config.sh
+++ bin/generate-solr-config.sh
@@ -119,8 +119,10 @@ else
 fi
 
 # Adapt autoSoftCommit to have a recommended value, and remove add-unknown-fields-to-the-schema
-sed -i '/<updateRequestProcessorChain name="add-unknown-fields-to-the-schema".*/,/<\/updateRequestProcessorChain>/d' $DESTINATION_DIR/solrconfig.xml
-sed -i 's/${solr.autoSoftCommit.maxTime:-1}/${solr.autoSoftCommit.maxTime:20}/' $DESTINATION_DIR/solrconfig.xml
+sed -i.bak '/<updateRequestProcessorChain name="add-unknown-fields-to-the-schema".*/,/<\/updateRequestProcessorChain>/d' $DESTINATION_DIR/solrconfig.xml
+sed -i.bak 's/${solr.autoSoftCommit.maxTime:-1}/${solr.autoSoftCommit.maxTime:20}/' $DESTINATION_DIR/solrconfig.xml
+
+rm $DESTINATION_DIR/solrconfig.xml.bak
 
 if [ "$GENERATE_SOLR_TMPDIR" != "" ]; then
     echo Removing temp dir: $GENERATE_SOLR_TMPDIR
