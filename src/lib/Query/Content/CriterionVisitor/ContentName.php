0be9b85a6649664240a3cbc62d2b4f0e9d2788e2
ciastektk <tomasz.kryszan@ez.no>
2024-01-17 14:55:16 +0100

    IBX-7418: Added CriterionVisitior for ContentName Criterion (#61)
    For more details see https://issues.ibexa.co/browse/IBX-7418 and https://github.com/ibexa/solr/pull/61
    
    Key changes:
    
    * Implemented criterion visitor for ContentName Criterion

---
+++ src/lib/Query/Content/CriterionVisitor/ContentName.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Solr\Query\Content\CriterionVisitor;
+
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Solr\Query\CriterionVisitor;
+
+/**
+ * @internal
+ */
+final class ContentName extends CriterionVisitor
+{
+    public function canVisit(Criterion $criterion): bool
+    {
+        return $criterion instanceof Criterion\ContentName
+            && $criterion->operator === Criterion\Operator::LIKE;
+    }
+
+    public function visit(Criterion $criterion, CriterionVisitor $subVisitor = null): string
+    {
+        /** @var string $value */
+        $value = $criterion->value;
+        $searchField = 'meta_content__name_s';
+
+        return "{!edismax v='{$this->escapeQuote($value)}' qf='{$searchField}' uf=-*}";
+    }
+}
