a91af10c9340ed5bccad4fdaa7e0ddf96dc02dcb
Tomasz Kryszan <tomasz.kryszan@ibexa.co>
2024-08-19 14:02:56 +0200

    Added location_bookmarked_user_ids search field

--- src/lib/FieldMapper/ContentFieldMapper/ContentDocumentLocationFields.php
+++ src/lib/FieldMapper/ContentFieldMapper/ContentDocumentLocationFields.php
@@ -6,6 +6,7 @@
  */
 namespace Ibexa\Solr\FieldMapper\ContentFieldMapper;
 
+use Ibexa\Contracts\Core\Persistence\Bookmark\Handler as BookmarkHandler;
 use Ibexa\Contracts\Core\Persistence\Content;
 use Ibexa\Contracts\Core\Persistence\Content\Location;
 use Ibexa\Contracts\Core\Persistence\Content\Location\Handler as LocationHandler;
@@ -23,8 +24,13 @@ class ContentDocumentLocationFields extends ContentFieldMapper
      */
     protected $locationHandler;
 
-    public function __construct(LocationHandler $locationHandler)
-    {
+    private BookmarkHandler $bookmarkHandler;
+
+    public function __construct(
+        BookmarkHandler $bookmarkHandler,
+        LocationHandler $locationHandler
+    ) {
+        $this->bookmarkHandler = $bookmarkHandler;
         $this->locationHandler = $locationHandler;
     }
 
@@ -87,6 +93,12 @@ class ContentDocumentLocationFields extends ContentFieldMapper
             $fields[] = new Field(
                 'location_ancestors',
                 $locationData['ancestors'],
+                new FieldType\MultipleIdentifierField()
+            );
+
+            $fields[] = new Field(
+                'location_bookmarked_user_ids',
+                $this->bookmarkHandler->loadUserIdsByLocation($location),
                 new FieldType\MultipleIdentifierField()
             );
         }
