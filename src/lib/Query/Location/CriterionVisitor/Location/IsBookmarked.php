85a65f5e038e99920670da30161168a9f4f2f3cf
Tomasz Kryszan <tomasz.kryszan@ibexa.co>
2024-08-20 09:52:43 +0200

    Fixed cs

--- src/lib/Query/Location/CriterionVisitor/Location/IsBookmarked.php
+++ src/lib/Query/Location/CriterionVisitor/Location/IsBookmarked.php
@@ -4,7 +4,6 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-
 namespace Ibexa\Solr\Query\Location\CriterionVisitor\Location;
 
 use Ibexa\Contracts\Core\Exception\InvalidArgumentException;
