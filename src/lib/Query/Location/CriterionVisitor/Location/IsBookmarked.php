6dbe0457a7e4be99565fd68b9d502f4ad47929fe
Tomasz Kryszan <tomasz.kryszan@ez.no>
2024-09-04 16:44:21 +0200

    IBX-8726: Added support for IsBookmarked criterion (#75)
    For more details see https://issues.ibexa.co/browse/IBX-8726 and https://github.com/ibexa/solr/pull/75
    
    Key changes:
    
    * Added IsBookmarked criterion visitor
    
    * Added location_bookmarked_user_ids search field
    
    * [Tests] Added IsBookmarkedTest
    
    ---------
    
    Co-Authored-By: Konrad Oboza <konradoboza@users.noreply.github.com>
    Co-Authored-By: Paweł Niedzielski <Steveb-p@users.noreply.github.com>

---
+++ src/lib/Query/Location/CriterionVisitor/Location/IsBookmarked.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Solr\Query\Location\CriterionVisitor\Location;
+
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Solr\Query\CriterionVisitor;
+use LogicException;
+
+final class IsBookmarked extends CriterionVisitor
+{
+    private const SEARCH_FIELD = 'location_bookmarked_user_ids_mid';
+
+    private PermissionResolver $permissionResolver;
+
+    public function __construct(PermissionResolver $permissionResolver)
+    {
+        $this->permissionResolver = $permissionResolver;
+    }
+
+    public function canVisit(Criterion $criterion): bool
+    {
+        return $criterion instanceof Criterion\Location\IsBookmarked
+            && $criterion->operator === Criterion\Operator::EQ;
+    }
+
+    public function visit(
+        Criterion $criterion,
+        CriterionVisitor $subVisitor = null
+    ): string {
+        if (!is_array($criterion->value)) {
+            throw new LogicException(sprintf(
+                'Expected %s Criterion value to be an array, received %s',
+                Criterion\Location\IsBookmarked::class,
+                get_debug_type($criterion->value),
+            ));
+        }
+
+        $userId = $this->permissionResolver
+            ->getCurrentUserReference()
+            ->getUserId();
+
+        $query = self::SEARCH_FIELD . ':"' . $userId . '"';
+
+        if (!$criterion->value[0]) {
+            $query = 'NOT ' . $query;
+        }
+
+        return $query;
+    }
+}
