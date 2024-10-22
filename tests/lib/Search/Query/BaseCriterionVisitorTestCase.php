d8da5ed495e1b4b10c22f2371a7a1fb128f81030
Tomasz Kryszan <tomasz.kryszan@ibexa.co>
2024-10-22 11:20:18 +0200

    [Tests] Added BaseCriterionVisitorTestCase

---
+++ tests/lib/Search/Query/BaseCriterionVisitorTestCase.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Solr\Search\Query;
+
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Solr\Query\CriterionVisitor;
+use PHPUnit\Framework\TestCase;
+
+abstract class BaseCriterionVisitorTestCase extends TestCase
+{
+    abstract protected function getVisitor(): CriterionVisitor;
+
+    abstract protected function getSupportedCriterion(): Criterion;
+
+    /**
+     * @return iterable<array{
+     *     string,
+     *     \Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion
+     * }>
+     */
+    abstract protected function provideDataForTestVisit(): iterable;
+
+    /**
+     * @dataProvider provideDataForTestCanVisit
+     */
+    public function testCanVisit(
+        bool $expected,
+        Criterion $criterion
+    ): void {
+        self::assertSame(
+            $expected,
+            $this->getVisitor()->canVisit($criterion)
+        );
+    }
+
+    /**
+     * @return iterable<array{
+     *     bool,
+     *     \Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion
+     * }>
+     */
+    public function provideDataForTestCanVisit(): iterable
+    {
+        yield 'Not supported criterion' => [
+            false,
+            new Criterion\ContentId(123),
+        ];
+
+        yield 'Supported criterion' => [
+            true,
+            $this->getSupportedCriterion(),
+        ];
+    }
+
+    /**
+     * @dataProvider provideDataForTestVisit
+     */
+    public function testVisit(
+        string $expectedQuery,
+        Criterion $criterion
+    ): void {
+        self::assertSame(
+            $expectedQuery,
+            $this->getVisitor()->visit($criterion)
+        );
+    }
+}
